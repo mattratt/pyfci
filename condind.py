89136f70a2dda5528bd92cb57810e764cfc517b0
Matthew J. H. Rattigan <mattratt@gmail.com>
2018-08-08 15:23:42 -0400

    added logic for cond indy tests with different types

--- condind.py
+++ condind.py
@@ -8,13 +8,61 @@ import logging
 from collections import defaultdict
 import numpy as np
 import pandas as pd
+from pandas.api.types import is_bool_dtype, is_categorical_dtype, is_numeric_dtype
 from scipy import stats
 
 
 logg = logging.getLogger(__name__)
 
 
+class CondIndTester(object):
+    def __init__(self, df):
+        self.df = df
+
+    def cond_ind(self, a_col_name, b_col_name, cond_col_names, alpha):
+        a = self.df[a_col_name]
+        b = self.df[b_col_name]
+        conds = [self.df[z] for z in cond_col_names]
+
+        if is_bool_dtype(a) and is_bool_dtype(b) and all([is_bool_dtype(z) for z in conds]):
+            stat, pval = cmh(a, b, conds)
+
+        elif (is_categorical_dtype(a) or is_bool_dtype(a)) and \
+                (is_categorical_dtype(b) or is_bool_dtype(b)) and \
+                all([is_categorical_dtype(z) or is_bool_dtype(z) for z in conds]):
+            stat, pval = chisq_3d(a, b, conds)
+
+        elif is_numeric_dtype(a) and \
+                is_numeric_dtype(b) and \
+                all([is_categorical_dtype(z) or is_bool_dtype(z) for z in conds]):
+            stat, pval = blocked_pearson(a, b, conds)
+
+        elif is_numeric_dtype(a) and \
+                is_numeric_dtype(b) and \
+                all([is_numeric_dtype(z) for z in conds]):
+            stat, pval = partial_corr_r(a, b, conds)
+
+        else:
+            stat, pval = freak out
+
+        return pval < alpha
+
+
+class CondIndOracle(CondIndTester):
+    zzz
+
+
 def partial_corr(xvals, yvals, condvals):
+    r = partial_corr_r(xvals, yvals, condvals)
+    # if we want the pval, we're going to need the zscore instead of r
+    # see: https://tinyurl.com/ycdbexzj
+    n = len(xvals)
+    z = r2zscore(r, n)
+    p = zscore_pval(z)
+    return r, p  #zzz should we be returning z here?
+
+
+def partial_corr_r(xvals, yvals, condvals):
     xvals = np.array(xvals)
     yvals = np.array(yvals)
     condvals = np.array(condvals)
@@ -25,21 +73,13 @@ def partial_corr(xvals, yvals, condvals):
     else:
         zvals = condvals[:, 0]
         condvals_rest = condvals[:, 1:]
-        rxy_rest = partial_corr(xvals, yvals, condvals_rest)
-        rxz_rest = partial_corr(xvals, zvals, condvals_rest)
-        rzy_rest = partial_corr(zvals, yvals, condvals_rest)
+        rxy_rest = partial_corr_r(xvals, yvals, condvals_rest)
+        rxz_rest = partial_corr_r(xvals, zvals, condvals_rest)
+        rzy_rest = partial_corr_r(zvals, yvals, condvals_rest)
         num = rxy_rest - (rxz_rest*rzy_rest)
         den = math.pow((1.0-rxz_rest**2)*(1.0-rzy_rest**2), 0.5)
         r = num / den
     return r
-
-
-def partial_corr_pval(r, n):
-    # if we want the pval, we're going to need the zscore instead of r
-    # see: https://tinyurl.com/ycdbexzj
-    z = r2zscore(r, n)
-    p = zscore_pval(z)
-    return p
 
 
 def r2zscore(r, n):
@@ -48,49 +88,9 @@ def r2zscore(r, n):
     return z
 
 
-def zscore_pval(z):
-    return 2.0*(1.0 - stats.norm.cdf(abs(z)))  #zzz should this be abs(z)?!
-
-
-# class Contingency:
-#     def __init__(self, row_vals, col_vals):
-#         self.n = 0
-#         self.counts = defaultdict(int)  # (row, col) -> count
-#         self.marginals_row = defaultdict(int)
-#         self.marginals_col = defaultdict(int)
-#         for val_tup in zip(row_vals, col_vals):
-#             self.n += 1
-#             self.counts[val_tup] += 1
-#             self.marginals_row[val_tup[0]] += 1
-#             self.marginals_col[val_tup[1]] += 1
-#
-#     def chisq(self):
-#         ret = 0.0
-#         for valRow, marginalRow in self.marginals_row.items():
-#             for valCol, marginalCol in self.marginals_col.items():
-#                 valTup = (valRow, valCol)
-#                 observed = self.counts.get(valTup, 0)
-#                 expected = marginalRow * marginalCol / float(self.n)
-#                 ret += (observed - expected) ** 2 / expected
-#         return ret
-#
-#     def gstat(self):
-#         ret = 0.0
-#         for valRow, marginalRow in self.marginals_row.items():
-#             for valCol, marginalCol in self.marginals_col.items():
-#                 valTup = (valRow, valCol)
-#                 observed = self.counts.get(valTup, 0)
-#                 if (observed > 0):
-#                     expected = marginalRow * marginalCol / float(self.n)
-#                     ret += 2 * observed * math.log(observed / expected)
-#         return ret
-#
-#     def dof(self):
-#         return (len(self.marginals_row) - 1) * (len(self.marginals_col) - 1)
-#
-#     def levels(self):
-#         return (len(self.marginals_row), len(self.marginals_col))
-
+def zscore_pval(z):  #zzz need to figure out correct calc
+    # return 2.0*(1.0 - stats.norm.cdf(abs(z)))
+    return -2*stats.norm.cdf(-np.abs(z))
 
 
 def chisq_3d(xvals, yvals, condvals):
